b82950a914e1a02a658833c1558655c4f4bffa79
Havallon <havallon.ecomp@gmail.com>
2018-01-08 17:03:28 -0200

    vga sprites

--- problema 4/vga driver/vgaDriver.v
+++ problema 4/vga driver/vgaDriver.v
@@ -1,10 +1,19 @@
 //VGA DRIVER
 // Crystal & Havallon
 // 02/12/2017
-module vgaDriver(clock_50MHz, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
+module vgaDriver(clock_50MHz, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B, x1, y1, x2, y2, xb, yb);
 	
 	input clock_50MHz;
 	input [11:0] KEY;
+	
+	input [9:0] x1;
+	input [9:0] y1;
+	
+	input [9:0] x2;
+	input [9:0] y2;
+	
+	input [9:0] xb;
+	input [9:0] yb;
 	
 	output VGA_HS;
 	output VGA_VS;
@@ -47,7 +56,7 @@ module vgaDriver(clock_50MHz, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
 	
 	vgaPxlGen vgaPxlGen (
 		.clk(clk25),
-		.frame_pulse(frame_pulse),
+		.frame_pulse(~frame_pulse),
 		.clk50(clock_50MHz),
 		.rst(KEY[0]),
 		.pxl_en(en),
@@ -55,7 +64,13 @@ module vgaDriver(clock_50MHz, KEY, VGA_HS, VGA_VS, VGA_R, VGA_G, VGA_B);
 		.y(y),
 		.r(rr),
 		.g(gg),
-		.b(bb)
+		.b(bb),
+		.x1(x1),
+		.y1(y1),
+		.x2(x2),
+		.y2(y2),
+		.xb(xb),
+		.yb(yb)
 	);
 	
 endmodule
